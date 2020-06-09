916b99dc634d640d570c5fbcbc1c847219f59057
jmoyson <moyson.jeremy@gmail.com>
2020-06-09 20:20:43 +0200

    Fix blogpost preview on netlify CMS

--- src/cms/preview-templates/BlogPostPreview.js
+++ src/cms/preview-templates/BlogPostPreview.js
@@ -1,24 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { BlogPostTemplate } from '../../templates/blog-post'
+import React from "react";
+import PropTypes from "prop-types";
+import { BlogPostTemplate } from "../../templates/blog-post";
+import { IntlProvider } from "react-intl";
+
+import fr from "../../data/messages/fr";
+import en from "../../data/messages/en";
 
 const BlogPostPreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
+  const tags = entry.getIn(["data", "tags"]);
+  const langKey = entry.getIn(["data", "langKey"]);
+  const messages = { fr, en };
+
   return (
-    <BlogPostTemplate
-      content={widgetFor('body')}
-      description={entry.getIn(['data', 'description'])}
-      tags={tags && tags.toJS()}
-      title={entry.getIn(['data', 'title'])}
-    />
-  )
-}
+    <IntlProvider locale={langKey} messages={messages[langKey]}>
+      <BlogPostTemplate
+        content={widgetFor("body")}
+        description={entry.getIn(["data", "description"])}
+        tags={tags && tags.toJS()}
+        title={entry.getIn(["data", "title"])}
+      />
+    </IntlProvider>
+  );
+};
 
 BlogPostPreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
   widgetFor: PropTypes.func,
-}
+};
 
-export default BlogPostPreview
+export default BlogPostPreview;
